271615381fa9ec75ac9b311ef0a117747bc4595d
William Desktop <chenweirongduke@gmail.com>
2017-05-15 22:49:01 -0400

    Geolocation navigator added

--- index.android.js
+++ index.android.js
@@ -11,19 +11,7 @@ import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 export default class FindYourBeach extends Component {
     render() {
         return (
-            <View style={styles.container}>
                 <Main />
-                <Text style={styles.welcome}>
-                    Yes me here
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.android.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
-                </Text>
-            </View>
         );
     }
 }
--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,26 @@ import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 import {Button} from 'react-native'
 
 class Main extends React.Component{
+    componentWillMount() {
+        console.log('pls')
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log('position', position);
+            let region = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 0.00922*1.5,
+                longitudeDelta: 0.00421*1.5
+            }
+
+            // this.onRegionChange(region, position.coords.accuracy);
+        });
+    }
     render() {
+        console.log('yas')
         return (
-            <View style={{flex:1}}>
-                <Text styles={styles.welcome}></Text>
-                <Button title="Find me a beach" color="#841584" accessibilityLabel="Learn more about this purple button"/>
+            <View style={styles.container}>
+                <Text style={styles.welcome}></Text>
+                <Button title="Find me a beach" color="#841584"/>
             </View>
         )
     }
